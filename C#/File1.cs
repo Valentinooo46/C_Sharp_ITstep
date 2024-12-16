3ba937c113a4650aa90a823d449fe5e98b7fb490
Valentinooo46 <valeartem@gmail.com>
2024-12-16 23:22:13 +0200

    Події.Делегати.Наслідування від інтерфейсів C#

---
+++ C#/File1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class File1
+    {
+        static void Main(string[] args)
+        {
+            List_Pets list1 = new List_Pets(10);
+            foreach (Pets item in list1) {
+                Console.WriteLine(item.Name.ToString() + " " + item.Spice);        
+            }
+            list1.See();
+        }
+    }
+}
