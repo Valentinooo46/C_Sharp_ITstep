a4512b04f249450eaffca716689a9a2e4a9fae06
Valentinooo46 <valeartem@gmail.com>
2025-01-20 20:34:06 +0200

    Серіалізація об'єктів.JSON/XML/Binary

--- C#/File1.cs
+++ C#/File1.cs
@@ -12,44 +12,11 @@ namespace Project2
     {
         static void Main(string[] args)
         {
-            Company comp = new Company();
-            comp.Add();
-            comp.Add();
-            comp.Add();
-            comp.Add();
-            var SalariesSortedList = from T in comp
-                                     orderby T.total_salaries descending
-                                     select T;
-            foreach(var salaries in SalariesSortedList)
-            {
-                Console.WriteLine("Весь заробіток відділу:" + salaries.total_salaries + "\nСередній заробіток відділу:" + salaries.average_salary + "\n---------------------------");
-            }
-            var SalariesUnitSortedList = from T in comp
-                                         from U in T
-                                     orderby U.total_salary descending
-                                     select U;
-            foreach(var units_salaries in SalariesUnitSortedList)
-            {
-                Console.WriteLine("Весь заробіток юніту:" + units_salaries.total_salary);
-            }
-            Console.WriteLine("----------------------\n");
-            var SalariesAverageSortedList = from T in comp
-                                     orderby T.max_salary - T.min_salary descending
-                                     select T;
-            foreach (var average_salaries in SalariesAverageSortedList)
-            {
-                Console.WriteLine("Розкид по зарплаті відділу:" + (average_salaries.max_salary - average_salaries.min_salary));
-            }
-            Console.WriteLine("----------------------\n");
-            var ShareMansSortedList = from T in comp
-                                            orderby T.share_mans descending
-                                            select T;
-            foreach (var share_mans in ShareMansSortedList)
-            {
-                Console.WriteLine("Частка чоловіків у відділі:" + (share_mans.share_mans * 100) + "%");
-            }
-            Console.WriteLine("----------------------\n");
+            MusicAlbum musicAlbum = MusicAlbum.LoadFromFile(@"C:\Users\valea\source\repos\Project2\Project2\MusicAlbum.json");
+            musicAlbum.SeeInfo();
+            Console.WriteLine(musicAlbum.SerializeToJson());
         }
+
         
     }
 }
