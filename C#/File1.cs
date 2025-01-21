a1b5a5144cea1fe249ee7af1ac2b094f3718529b
Valentinooo46 <valeartem@gmail.com>
2025-01-21 18:09:37 +0200

    Структурні паттерни у C#

--- C#/File1.cs
+++ C#/File1.cs
@@ -12,11 +12,69 @@ namespace Project2
     {
         static void Main(string[] args)
         {
-            MusicAlbum musicAlbum = MusicAlbum.LoadFromFile(@"C:\Users\valea\source\repos\Project2\Project2\MusicAlbum.json");
-            musicAlbum.SeeInfo();
-            Console.WriteLine(musicAlbum.SerializeToJson());
+            ITarget adapter = new Adapter();
+            adapter.ClientDouble(1.5); 
+            adapter.ClientInt(2);      
+            adapter.ClientChar('A');   
         }
 
-        
+
     }
+    interface ITarget
+    {
+        void ClientDouble(Double val);
+        void ClientInt(Int32 val);
+        void ClientChar(Char val);
+    }
+    class Adapter :Original,ITarget
+    {
+        
+        public void ClientDouble(Double val)
+        {
+            OriginalDouble(val * 2);
+        }
+        public void ClientInt(Int32 val)
+        {
+            OriginalInt(val * 4);
+        }
+        public void ClientChar(Char val)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                OriginalChar(val);
+            }
+           
+        }
+    }
+    class Original
+    {
+        public void OriginalDouble(Double val)
+        {
+            Console.WriteLine($"{val}");
+        }
+        public void OriginalInt(Int32 val)
+        {
+            Console.WriteLine($"{val}");
+        }
+        public void OriginalChar(Char val)
+        {
+            Console.WriteLine($"{val}");
+        }
+    }
+    class Client: ITarget
+    {
+        public void ClientDouble(Double val)
+        {
+            Console.WriteLine($"{val}");
+        }
+        public void ClientInt(Int32 val)
+        {
+            Console.WriteLine($"{val}");
+        }
+        public void ClientChar(Char val)
+        {
+            Console.WriteLine($"{val}");
+        }
+    }
+
 }
